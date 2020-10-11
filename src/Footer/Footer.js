e8d78c3f807666672b2df1750032f1bd73f4cd98
Nusrath Fathima <nfathima@uncc.edu>
2020-10-11 01:26:22 -0400

    Brought PBudget code in

---
+++ src/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className="bottom">
+        <section className="center">
+            All rights reserved &copy; Nusrath Fathima
+        </section>
+      </footer>
+  );
+}
+
+export default Footer;
---
+++ src/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Hero() {
+  return (
+    <main id="main" className="hero" title="Personal Budget App">
+        <h1>Personal Budget</h1>
+        <h2>A personal-budget management app</h2>
+    </main>
+  );
+}
+
+export default Hero;
